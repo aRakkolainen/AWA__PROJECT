154ac3f773e10ff4f206a95e571c8b0c3a32d1dd
aRakkolainen <aino.rakkolainen@gmail.com>
2024-02-21 09:33:30 +0200

    Update ShowUsers.jsx

--- client/src/components/MainPage/ShowUsers.jsx
+++ client/src/components/MainPage/ShowUsers.jsx
@@ -1,17 +1,47 @@
 import {useState, useEffect} from 'react';
 import UserItem from './UserItem';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
+//import Button from 'react-bootstrap/Button';
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer, toast } from "react-toastify";;
+import { ToastContainer, toast } from "react-toastify";
+;
 //import Header from '../Header/Header';
 
 //Showing toast message when match is found: https://blog.logrocket.com/using-react-toastify-style-toast-messages/
 
 const ShowUsers = function(props) {
     const [userData, setUserData] = useState([]);
+    const [users, setUsers] = useState([]); 
     let [currUserIndex, setCurrentUserIndex] = useState(0);
     let [likedUsers, setLikedUsers] = useState([]);
+    
+    const mapUsers = (likedUsers) => {
+        let users = [];
+        userData.map((user) => {
+            // All users besides the logged in user are saved into this list
+            if (user.username !== localStorage.getItem("username")) { 
+                if (likedUsers.length > 0) {
+                    if (likedUsers.includes("No liked users")) {
+                        console.log("No liked users");
+                        users.push(user);
+                    }
+                    else {
+                        console.log("This user has liked users, checking them..")
+                        for (let i=0; i< likedUsers.length; i++) {
+                            //console.log(likedUsers[i]);
+                            if (likedUsers[i] === user.username) {
+                                console.log("Already liked this user, hiding them!");
+                            } else {
+                                users.push(user);
+                            }
+                        }
+                    }
+                } 
+    
+        }
+    })
+    return users; 
+    }
     //Fetching all user profiles
     useEffect(() => {
         fetch("/api/user/list", { method:"GET",
@@ -32,6 +62,9 @@ const ShowUsers = function(props) {
         .then(response => response.json())
         .then(json => setLikedUsers(json.likedUsersList))
     }, [])
+    useEffect(()=> {
+        setUsers(mapUsers(likedUsers));
+    }, [likedUsers])
 
     //Function for liking user and saving it to friends list and friend
     const handleLike = (username) => {
@@ -74,37 +107,10 @@ const ShowUsers = function(props) {
 
     }
 
-    const mapUsers = (likedUsers) => {
-        let users = [];
-        userData.map((user) => {
-            // All users besides the logged in user are saved into this list
-            if (user.username !== localStorage.getItem("username")) { 
-                if (likedUsers.length > 0) {
-                    if (likedUsers.includes("No liked users")) {
-                        console.log("No liked users");
-                        users.push(user);
-                    }
-                    else {
-                        console.log("This user has liked users, checking them..")
-                        for (let i=0; i< likedUsers.length; i++) {
-                            //console.log(likedUsers[i]);
-                            if (likedUsers[i] === user.username) {
-                                console.log("Already liked this user, hiding them!");
-                            } else {
-                                users.push(user);
-                            }
-                        }
-                    }
-                } 
     
-        }
-        return users; 
-    })
-    return users; 
-    }
     //Creating list of users except the logged in user! Also checking who the user has already liked/added to be friends so not showing same ones again!
-    let users = mapUsers(likedUsers);
     //Creating list of found users
+    console.log(users);
     const userList = users.map((user) => {
         if (user.username) {
             return <UserItem username={user.username} bio={user.bio} like={handleLike} dislike={handleDisLike}></UserItem> 
